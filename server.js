87e4555c7fc1ed1c992185638a3e6d635a4e277a
Carlos <fariascarlos2346@gmail.com>
2025-02-05 23:03:10 -0300

    duas rotas criadas, listando e criando usuarios

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = [] // nosso DBA
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
 
     res.status(200).json(users)
 })
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
+app.post('/usuarios', async (req, res) => {
 
-    res.status(201).json({ message: "Usúario criado com sucesso"})
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+
+    res.status(201).json(user)
 })
 
 app.listen(3000)
 
 
-karla197523
-AmeJylMqNZHxap0m
+//karla197523
+//AmeJylMqNZHxap0m
 
 
-YRj05bXgB9bfTsuA
-Cacula
+//YRj05bXgB9bfTsuA
+//Cacula
